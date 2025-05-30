498f6cf3208dc3fd06bffef024bde905f0694e55
avinashkapudasi <avinashkapudasi@gmail.com>
2025-05-30 09:37:53 +0530

    reload fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  build: {
-    assetsInlineLimit: 0, // Prevent inlining small assets
-    rollupOptions: {
-      input: {
-        main: 'index.html', // Ensure entry points are defined
-      },
-      preserveEntrySignatures: 'strict', // Preserve unused assets
-    },
-  },
-});
+  base: '/',
+})
